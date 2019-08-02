77964f4eb385fa4b45d92a91e8fac6204b2cb994
Artem Kornev <artemkakornev1@gmail.com>
2019-08-02 18:08:47 +0300

    m

--- src/services/dummy-unsplash-service.js
+++ src/services/dummy-unsplash-service.js
@@ -1,16 +1,16 @@
 export default class DummyUnsplashService {
-  counter = 0;
+  itemId = 0;
 
   getRandomPhotos = (count) => {
     const result = [];
 
     for (let i = 0; i < count; i++) {
-      this.counter++;
+      this.itemId++;
       result.push({
         alt: 'TEST-alt',
         description: 'Test-Description',
         color: '#EEE',
-        id: this.counter,
+        id: this.itemId,
         urlThumb:
           'http://www.mediacy.com/images/home/Image-Pro_Icon_256x256.png',
         urlDownload:
