7b7c589f1883175ee6c0b90b566382c6952adbe7
Artem Kornev <artemkakornev1@gmail.com>
2019-08-07 14:46:53 +0300

    Updates

---
+++ precache-manifest.bf9f08edf38d345dbe20b89be29f0fdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168635c0d41d433b7cd5e5d2ed1ae83f",
+    "url": "/unsplash-gallery/index.html"
+  },
+  {
+    "revision": "5e005460d1247f82fe25",
+    "url": "/unsplash-gallery/static/css/main.d63a3cfa.chunk.css"
+  },
+  {
+    "revision": "478c2278094423424377",
+    "url": "/unsplash-gallery/static/js/2.3051047a.chunk.js"
+  },
+  {
+    "revision": "5e005460d1247f82fe25",
+    "url": "/unsplash-gallery/static/js/main.b49a7ab6.chunk.js"
+  },
+  {
+    "revision": "5e7e71b1635519c0a2cb",
+    "url": "/unsplash-gallery/static/js/runtime~main.abf401f0.js"
+  }
+]);
