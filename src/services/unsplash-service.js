eaea897886c7b7229e59ba27da17c5206e97e2f7
Artem Kornev <artemkakornev1@gmail.com>
2019-09-15 18:27:44 +0300

    ь

--- src/services/unsplash-service.js
+++ src/services/unsplash-service.js
@@ -8,12 +8,6 @@ export default class UnsplashService extends Unsplash {
       secret:
         '200db19660f8603baed20d25918dbc15053b86255dcf2ac3baa705d95d5f74b8',
     });
-    // super({
-    //   applicationId:
-    //     'ab3411e4ac868c2646c0ed488dfd919ef612b04c264f3374c97fff98ed253dc9 ',
-    //   secret:
-    //     '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043',
-    // });
   }
 
   getResourse = async (method, ...args) => {
